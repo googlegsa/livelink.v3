4a38ef7f530cabd88765688a8b77188283cdb158
John Lacey <jlacey@google.com>
2011-09-21 06:27:27 +0000

    Make the Livelink base URL used by HttpURLContentHandler configurable.
    Revision 497 made the URL path configurable. This builds on that (and
    changes the property name) to allow the documents to be retrieved from
    a different Livelink front-end than used for the display URLs.
    
    HttpURLContentHandler now has two configurable properties: urlBase
    (default: Livelink URL) and urlPath (default: objAction=download).
    
    Also removed a log message that included the LLCookie value, which
    essentially acts as a password.
    
    Code review: http://codereview.appspot.com/5082043/
    
    M    projects/otex-core/source/java/com/google/enterprise/connector/otex/HttpURLContentHandler.java

--- projects/otex-core/source/java/com/google/enterprise/connector/otex/HttpURLContentHandler.java
+++ projects/otex-core/source/java/com/google/enterprise/connector/otex/HttpURLContentHandler.java
@@ -47,8 +47,11 @@ class HttpURLContentHandler implements ContentHandler {
     /** The user's LLCookie value. */
     private String llCookie;
 
-    /** The data URL pattern. */
-    String baseUrl = "?func=ll&objAction=download&objId=";  
+    /** The Livelink base URL. The default is the configured Livelink URL. */
+    String urlBase;  
+
+    /** The document URL path. */
+    String urlPath = "?func=ll&objAction=download&objId=";  
 
     HttpURLContentHandler() {
     }
@@ -60,19 +63,29 @@ class HttpURLContentHandler implements ContentHandler {
         this.client = client;
 
         llCookie = getLLCookie();
-        if (LOGGER.isLoggable(Level.FINE))
-            LOGGER.fine("LLCOOKIE: " + llCookie);
+        urlBase = connector.getDisplayUrl();
     }
 
     /**
-     * Sets the baseUrl property. 
+     * Sets the document base URL. 
      *
-     * @param baseUrl the URL pattern for data retrieval; by
+     * @param urlBase the Livelink base URL for document retrieval; by
+     * default the configured Livelink URL
+     * @since 3.0
+     */
+    public void setUrlBase(String urlBase) {
+        this.urlBase = urlBase; 
+    }
+
+    /**
+     * Sets the document URL path, which is appended to the base URL.
+     *
+     * @param urlPath the URL path for document retrieval; by
      * default "?func=ll&objAction=download&objId="
      * @since 3.0
      */
-    public void setBaseUrl(String baseUrl) {
-        this.baseUrl = baseUrl; 
+    public void setUrlPath(String urlPath) {
+        this.urlPath = urlPath; 
     }
 
     /**
@@ -96,9 +109,7 @@ class HttpURLContentHandler implements ContentHandler {
     public InputStream getInputStream(int volumeId, int objectId,
             int versionNumber, int size) throws RepositoryException {
         try {
-            String downloadUrlBase = connector.getDisplayUrl() +
-                baseUrl; 
-            URL downloadUrl = new URL(downloadUrlBase + objectId);
+            URL downloadUrl = new URL(urlBase + urlPath + objectId);
             if (LOGGER.isLoggable(Level.FINEST))
                 LOGGER.fine("DOWNLOAD URL: " + downloadUrl);
             URLConnection download = downloadUrl.openConnection();
