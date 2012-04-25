57ee4196d9be260f0f8e63dc8656044facf3d5b3
John Lacey <jlacey@google.com>
2012-04-25 04:29:14 +0000

    Fixed compilation error against CM 3.x.
    Code review: http://codereview.appspot.com/6123047/

--- projects/otex-lapi/source/javatests/com/google/enterprise/connector/otex/MockFileFeedConnection.java
+++ projects/otex-lapi/source/javatests/com/google/enterprise/connector/otex/MockFileFeedConnection.java
@@ -1,10 +1,10 @@
-// Copyright (C) 2007-2008 Google Inc.
+// Copyright 2007 Google Inc.
 //
 // Licensed under the Apache License, Version 2.0 (the "License");
 // you may not use this file except in compliance with the License.
 // You may obtain a copy of the License at
 //
-//      http://www.apache.org/licenses/LICENSE-2.0
+//     http://www.apache.org/licenses/LICENSE-2.0
 //
 // Unless required by applicable law or agreed to in writing, software
 // distributed under the License is distributed on an "AS IS" BASIS,
@@ -39,14 +39,18 @@ public class MockFileFeedConnection implements FeedConnection {
     return "Success";
   }
 
-
-  //@Override
+  @Override
   public boolean isBacklogged() {
     return false;
   }
 
-  //@Override
+  @Override
   public String getContentEncodings() {
     return "base64binary";
+  }
+
+  @Override
+  public boolean supportsInheritedAcls() {
+    return true;
   }
 }
