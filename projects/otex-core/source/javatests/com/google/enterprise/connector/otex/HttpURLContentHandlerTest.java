922c52cfabceb7bf7128b0fe0c686859fb8ac1bc
John Lacey <jlacey@google.com>
2013-06-16 04:49:04 +0000

    Fix issue 9444414: HttpURLContentHandler should support timeouts.
    Added properties for the connect and read timeouts, which are passed
    through to the URLConnection. I added unit tests for the timeouts, to
    ensure that an exception is thrown while reading the headers or body
    of the response. I added a TODO to verify that these exceptions lead
    to skipping the document in both cases, which I verified manually.
    
    Other changes:
    
    * Removed the unused connector field.
    
    * Made the urlBase and urlPath fields private.
    
    Code review: https://codereview.appspot.com/10309043

--- projects/otex-core/source/javatests/com/google/enterprise/connector/otex/HttpURLContentHandlerTest.java
+++ projects/otex-core/source/javatests/com/google/enterprise/connector/otex/HttpURLContentHandlerTest.java
@@ -18,7 +18,17 @@ import com.google.enterprise.connector.otex.client.Client;
 import com.google.enterprise.connector.otex.client.ClientFactory;
 import com.google.enterprise.connector.spi.RepositoryException;
 
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+import com.sun.net.httpserver.HttpServer;
+
 import junit.framework.TestCase;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.InetSocketAddress;
+import java.net.SocketTimeoutException;
 
 public class HttpURLContentHandlerTest extends TestCase {
   private LivelinkConnector connector;
@@ -55,5 +65,121 @@ public class HttpURLContentHandlerTest extends TestCase {
     out.initialize(connector, client);
 
     assertEquals(expected, out.getUrlPath());
+  }
+
+  public void testConnectTimeout() {
+    assertEquals(0, out.getConnectTimeout());
+    int expected = 60;
+    out.setConnectTimeout(expected);
+
+    assertEquals(expected * 1000, out.getConnectTimeout());
+  }
+
+  // TODO(jlacey): Add a live test of the connect timeout. Maybe we
+  // can use non-blocking I/O and not call select?
+
+  public void testReadTimeout() {
+    assertEquals(0, out.getReadTimeout());
+    int expected = 60;
+    out.setReadTimeout(expected);
+
+    assertEquals(expected * 1000, out.getReadTimeout());
+  }
+
+  /** In practice, this timeout exception will be caught in the connector. */
+  /* TODO(jlacey): Verify that the timeout exception skips the document. */
+  public void testReadTimeoutInHeaders()
+      throws RepositoryException, IOException {
+    // Sleep 2 seconds and timeout after 1 second.
+    out.setReadTimeout(1);
+    HttpServer server = createSleepyServer(2, SleepyLocation.HEADERS);
+    server.start();
+
+    try {
+      out.getInputStream(0, 0, 0, 0);
+      fail("Expected a SocketTimeoutException");
+    } catch (LivelinkException expected) {
+      assertNotNull(expected.toString(), expected.getCause());
+      assertTrue(expected.getCause().toString(),
+          expected.getCause() instanceof SocketTimeoutException);
+    } finally {
+      server.stop(0);
+    }
+  }
+
+  /**
+   * In practice, this timeout exception will be caught in the
+   * connector manager.
+   */
+  /* TODO(jlacey): Verify that the timeout exception skips the document. */
+  public void testReadTimeoutInBody() throws RepositoryException, IOException {
+    // Sleep 2 seconds and timeout after 1 second.
+    out.setReadTimeout(1);
+    HttpServer server = createSleepyServer(2, SleepyLocation.BODY);
+    server.start();
+
+    InputStream in = out.getInputStream(0, 0, 0, 0);
+    try {
+      in.read();
+      fail("Expected a SocketTimeoutException");
+    } catch (SocketTimeoutException expected) {
+    } finally {
+      server.stop(0);
+    }
+  }
+
+  public enum SleepyLocation { HEADERS, BODY };
+
+  /**
+   * Creates a sleepy HttpServer and configures the object under test to
+   * talk to it.
+   */
+  private HttpServer createSleepyServer(int sleepSeconds, SleepyLocation where)
+      throws RepositoryException, IOException {
+    // TODO(jlacey): Get an unused port.
+    String host = "localhost";
+    int port = 58080;
+    String path = "/";
+
+    out.setUrlBase("http://" + host + ":" + port);
+    out.setUrlPath(path);
+    out.initialize(connector, client);
+
+    HttpServer server = HttpServer.create(new InetSocketAddress(host, port), 0);
+    server.createContext(path, new SleepyHandler(sleepSeconds, where));
+    return server;
+  }
+
+  static class SleepyHandler implements HttpHandler {
+    private final int sleepSeconds;
+    private final SleepyLocation where;
+
+    public SleepyHandler(int sleepSeconds, SleepyLocation where) {
+      this.sleepSeconds = sleepSeconds;
+      this.where = where;
+    }
+
+    public void handle(HttpExchange exchange) throws IOException {
+      if (where == SleepyLocation.HEADERS) {
+        sleep();
+      }
+      byte[] response = "hello, world".getBytes();
+      exchange.sendResponseHeaders(200, response.length);
+      OutputStream body = exchange.getResponseBody();
+
+      if (where == SleepyLocation.BODY) {
+        sleep();
+      }
+      body.write(response);
+      body.close();
+    }
+
+    private void sleep() {
+      try {
+        Thread.sleep(sleepSeconds * 1000);
+      } catch (InterruptedException e) {
+        // TODO(jlacey): Rethrow as an InterruptedIOException?
+      }
+    }
   }
 }
